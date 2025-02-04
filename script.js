e12ccaa1b0249e84421350d2ac6b0c943c1b22b9
Kelsee Lawrence <klawrence1@ewu.edu>
2025-02-04 22:52:39 +0000

    HTML, CSS, JS

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-// website loaded?
+// ✅ Add External ColorKit Library
+// Color Kit Library
+https://codepen.io/manikoth/pen/NPKVOxm.js
 
-console.log("Okay");
+// Get Stuff
+const elBody = document.querySelector('body');
+
+// Set variable for HTML `body` element
+const elBody = document.querySelector("body");
+
+// ✅ Check-Check: log out the `body` element in the console
+console.log(elBody);
+
+// ✅ Check-Check: get and attach the node for the background color
+console.log(elBody.style.backgroundColor);
+
+// ✅ Hard code a background color (string)
+elBody.style.backgroundColor = "lightblue"; // Example color
+
+// ✅ Dynamically set background color using ColorKit library
+elBody.style.backgroundColor = colorkit.random();
+
+// ✅ Add a sparkle effect for fun
+colorkit.sparkle(elBody);
